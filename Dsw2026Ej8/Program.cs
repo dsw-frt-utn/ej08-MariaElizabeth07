1676b11332f8ce7b6dc9f331e57f9acfbe78baf8
Maria Morales <mareli18morales@gmail.com>
2026-05-04 16:57:41 -0300

    Solucion Ejercicio 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -13,8 +13,18 @@
 
             string etiqueta = helper.ObtenerEtiquetaProducto(code, description, price);
 
-            Console.WriteLine("     Etiqueta:");
+            Console.WriteLine("    Etiqueta:");
             Console.WriteLine(etiqueta);
+
+            Problema2 p2 = new Problema2();
+
+            int quantity = 2;
+
+            string resultado = p2.CrearResumenVenta(code, description, quantity, price);
+
+            Console.WriteLine("\n    Resumen de Venta:");
+            Console.WriteLine(resultado);
+
 
         }
     }
